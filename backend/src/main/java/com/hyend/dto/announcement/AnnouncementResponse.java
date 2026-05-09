b95e10bbf9b514f49c3389ff1f54f520540bcfda
sss1-yyy <ksui9210@gmail.com>
2026-05-09 16:35:35 +0900

    feat:announcement dto  구현

--- backend/src/main/java/com/hyend/dto/announcement/AnnouncementResponse.java
+++ backend/src/main/java/com/hyend/dto/announcement/AnnouncementResponse.java
@@ -1,5 +1,20 @@
 package com.hyend.dto.announcement;
 
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
 // TODO [H-1] 공지사항 상세 응답 DTO 구현
-public record AnnouncementResponse() {
+public record AnnouncementResponse(
+        Long id,
+        String title,
+        String content,
+        String category,
+        String writer,
+        boolean isImportant,
+        int viewCount,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+
+) {
 }
