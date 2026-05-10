6a09d3a032c170b3e330adb4a269cf4283298c17
sss1-yyy <ksui9210@gmail.com>
2026-05-10 19:43:28 +0900

    feat:inquiry dto 구현

--- backend/src/main/java/com/hyend/dto/inquiry/InquiryRequest.java
+++ backend/src/main/java/com/hyend/dto/inquiry/InquiryRequest.java
@@ -1,5 +1,14 @@
 package com.hyend.dto.inquiry;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 // TODO [H-1] 문의 생성 요청 DTO 구현
-public record InquiryRequest() {
+public record InquiryRequest(
+
+        @NotBlank @Size(max=100) String title,
+        @NotBlank String content,
+        @NotBlank String category
+
+) {
 }
