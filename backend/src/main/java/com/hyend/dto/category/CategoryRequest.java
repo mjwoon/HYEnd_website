66b025dc317d283802aa41ae29104e8ff7f7f83a
sss1-yyy <ksui9210@gmail.com>
2026-05-13 16:06:21 +0900

    Feat/category (#6)
    * feat: 의존성 추가 및 DTO 클래스 생성
    
    * feat:book dto 추가
    
    * feat:category dto구현

--- backend/src/main/java/com/hyend/dto/category/CategoryRequest.java
+++ backend/src/main/java/com/hyend/dto/category/CategoryRequest.java
@@ -1,5 +1,10 @@
 package com.hyend.dto.category;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 // TODO [H-1] 카테고리 생성/수정 요청 DTO 구현
-public record CategoryRequest() {
+public record CategoryRequest(
+        @NotBlank @Size(max=20) String name
+) {
 }
