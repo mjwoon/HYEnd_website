a034aae92de5e19941aa2752ba7d2bcc7c2619e8
sss1-yyy <ksui9210@gmail.com>
2026-05-15 02:31:37 +0900

    feat: Mapperì¶ 추가 및 dto-file,inquiry ì브랜mapper 브랜치로 추가

--- backend/src/main/java/com/hyend/mapper/AnnouncementMapper.java
+++ backend/src/main/java/com/hyend/mapper/AnnouncementMapper.java
@@ -1,5 +1,16 @@
 package com.hyend.mapper;
 
+import com.hyend.dto.announcement.AnnouncementRequest;
+import com.hyend.dto.announcement.AnnouncementResponse;
+import com.hyend.dto.announcement.AnnouncementSummary;
+import com.hyend.entity.Announcement;
+import org.mapstruct.Mapper;
+
 // TODO [H-3] 공지사항 MapStruct Mapper 구현
+@Mapper(componentModel = "spring")
 public interface AnnouncementMapper {
+        Announcement toEntity(AnnouncementRequest request);
+        AnnouncementResponse toResponse(Announcement announcement);
+        AnnouncementSummary toSummary(Announcement announcement);
+
 }
