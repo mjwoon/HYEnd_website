b85299be10134138202c3d7be0e6e86bdf28a490
rooftop <jmkjmk6666@gmail.com>
2026-05-14 16:31:28 +0900

    feat: 프론트엔드 초기 설정 및 공통 레이아웃 구현
    - motion, styled-components, zustand 라이브러리 설정
    - Pretendard Variable 폰트 적용 (12/14/15/28/40px, Medium/Bold)
    - 다크 테마 적용 (네온그린 #5FFB7A, 블랙 #0A0A0A)
    - RootLayout: 배경 동심원, Header, Footer, Outlet 구조
    - Header: About/Board 드롭다운 중앙 정렬, 활성 경로 표시
    - 전체 라우팅 14개 페이지 설정

--- frontend/src/pages/about/GreetingPage.tsx
+++ frontend/src/pages/about/GreetingPage.tsx
@@ -5,6 +5,6 @@ const Wrapper = styled.div`
   color: white;
 `;
 
-export default function HomePage() {
-  return <Wrapper>HomePage</Wrapper>;
+export default function GreetingPage() {
+  return <Wrapper>GreetingPage</Wrapper>;
 }
