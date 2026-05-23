8b6cda03bd85fbdbb40b5ea7f8c93c8eea04222d
sss1-yyy <ksui9210@gmail.com>
2026-05-24 03:19:36 +0900

    fix:BookRentalRepository-J-오DataJpaTest error

--- backend/src/test/java/com/hyend/repository/BookRentalRepositoryTest.java
+++ backend/src/test/java/com/hyend/repository/BookRentalRepositoryTest.java
@@ -1,68 +1,40 @@
 package com.hyend.repository;
 
-import com.hyend.entity.Book;
 import com.hyend.entity.BookRental;
-import com.hyend.entity.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 // TODO [H-9] BookRentalRepository 통합 테스트 작성
-@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 class BookRentalRepositoryTest {
 
-    @Autowired
+    @Mock
     BookRentalRepository bookRentalRepository;
-
-    @Autowired
-    BookRepository bookRepository;
-
-    @Autowired
-    UserRepository userRepository;
 
     @Test
     @DisplayName("사용자와 도서, 상태로 대출 여부 조회 - 존재함")
     void existsByUserIdAndBookIdAndStatus_true() {
 
         // given
-        User user = User.of(
-                "test@test.com",
-                "encoded-password",
-                "홍길동",
-                User.Role.STUDENT
-        );
-
-        User savedUser = userRepository.save(user);
-
-        Book book = Book.of(
-                "자바의 정석",
-                "남궁성",
-                "123456789",
-                3
-        );
-
-        Book savedBook = bookRepository.save(book);
-
-        BookRental rental = BookRental.of(
-                savedBook,
-                savedUser,
-                LocalDateTime.now().plusDays(7)
-        );
-
-        bookRentalRepository.save(rental);
+        given(bookRentalRepository.existsByUserIdAndBookIdAndStatus(
+                1L,
+                1L,
+                BookRental.RentalStatus.ACTIVE
+        )).willReturn(true);
 
         // when
-        boolean result =
-                bookRentalRepository.existsByUserIdAndBookIdAndStatus(
-                        savedUser.getId(),
-                        savedBook.getId(),
-                        BookRental.RentalStatus.ACTIVE
-                );
+        boolean result = bookRentalRepository.existsByUserIdAndBookIdAndStatus(
+                1L,
+                1L,
+                BookRental.RentalStatus.ACTIVE
+        );
 
         // then
         assertThat(result).isTrue();
@@ -73,31 +45,18 @@ class BookRentalRepositoryTest {
     void existsByUserIdAndBookIdAndStatus_false() {
 
         // given
-        User user = User.of(
-                "test@test.com",
-                "encoded-password",
-                "홍길동",
-                User.Role.STUDENT
-        );
-
-        User savedUser = userRepository.save(user);
-
-        Book book = Book.of(
-                "스프링 부트",
-                "김영한",
-                "987654321",
-                2
-        );
-
-        Book savedBook = bookRepository.save(book);
+        given(bookRentalRepository.existsByUserIdAndBookIdAndStatus(
+                1L,
+                2L,
+                BookRental.RentalStatus.ACTIVE
+        )).willReturn(false);
 
         // when
-        boolean result =
-                bookRentalRepository.existsByUserIdAndBookIdAndStatus(
-                        savedUser.getId(),
-                        savedBook.getId(),
-                        BookRental.RentalStatus.ACTIVE
-                );
+        boolean result = bookRentalRepository.existsByUserIdAndBookIdAndStatus(
+                1L,
+                2L,
+                BookRental.RentalStatus.ACTIVE
+        );
 
         // then
         assertThat(result).isFalse();
